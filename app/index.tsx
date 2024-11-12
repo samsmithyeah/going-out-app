c9204a13ba6aee1368f125651291fbf5e3622d91
Sam <sam.smith@red-badger.com>
2024-11-12 15:14:18 +0000

    Fix notification permission request

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,10 @@ import { UserProvider } from '../context/UserContext';
 import { CrewsProvider } from '../context/CrewsContext';
 import { InvitationsProvider } from '../context/InvitationsContext';
 import { registerRootComponent } from 'expo';
+import { LogBox } from 'react-native';
 import App from './App';
+
+LogBox.ignoreLogs(['Sending `onAnimatedValueUpdate` with no listeners registered.']);
 
 const Root: React.FC = () => {
   return (
