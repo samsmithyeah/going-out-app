f3bef93a7de987abd680de1c0bde9dd3d3a5c5e8
Sam <sam.smith@red-badger.com>
2024-11-15 13:00:50 +0000

    Custom styles for toast

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,68 @@ import { UserProvider } from '../context/UserContext';
 import { CrewsProvider } from '../context/CrewsContext';
 import { InvitationsProvider } from '../context/InvitationsContext';
 import { registerRootComponent } from 'expo';
-import Toast from 'react-native-toast-message';
+import Toast, {
+  BaseToast,
+  ErrorToast,
+  InfoToast,
+  ToastProps,
+} from 'react-native-toast-message';
 import { LogBox } from 'react-native';
 import App from './App';
 
 LogBox.ignoreLogs([
   'Sending `onAnimatedValueUpdate` with no listeners registered.',
 ]);
+
+const toastConfig = {
+  success: (props: ToastProps) => (
+    <BaseToast
+      {...props}
+      text1Style={{
+        fontSize: 15,
+        fontWeight: '400',
+      }}
+      text2Style={{
+        fontSize: 13,
+      }}
+      style={{
+        borderLeftColor: '#008000',
+      }}
+    />
+  ),
+
+  error: (props: ToastProps) => (
+    <ErrorToast
+      {...props}
+      text1Style={{
+        fontSize: 15,
+        fontWeight: '400',
+      }}
+      text2Style={{
+        fontSize: 13,
+      }}
+      style={{
+        borderLeftColor: '#FF0000',
+      }}
+    />
+  ),
+
+  info: (props: ToastProps) => (
+    <InfoToast
+      {...props}
+      text1Style={{
+        fontSize: 15,
+        fontWeight: '400',
+      }}
+      text2Style={{
+        fontSize: 13,
+      }}
+      style={{
+        borderLeftColor: '#FFA500',
+      }}
+    />
+  ),
+};
 
 const Root: React.FC = () => {
   return (
@@ -20,7 +75,7 @@ const Root: React.FC = () => {
           </InvitationsProvider>
         </CrewsProvider>
       </UserProvider>
-      <Toast />
+      <Toast config={toastConfig} />
     </>
   );
 };
